8a76edb810caa9eda169ff95ee05afcadd99eba5
Brandon Nichols <brandonaronnichols@gamil.com>
2019-11-01 15:28:24 -0500

    added get request in postRouter that obtains all the comments related to a specific post

--- post-router.js
+++ post-router.js
@@ -12,10 +12,10 @@ postRouter.post('/', (req, res) => {
             if(reqBody.title && reqBody.contents)
                 res.status(201).json({reqBody});
             else
-                res.status(400).json({errorMessage: "Please provide title and contents for the post."});
+                res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
         })
         .catch(err => {
-            res.status(500).json({error: "There was an error while saving the post to the database"});
+            res.status(500).json({ error: "There was an error while saving the post to the database" });
         });
 });
 
@@ -29,16 +29,16 @@ postRouter.post('/:id/comments', (req, res) => {
                 if(reqBody.text){
                     res.status(201).json(reqBody);
                 } else{
-                    res.status(400).json({errorMessage: "Please provide text for the comment."});
+                    res.status(400).json({ errorMessage: "Please provide text for the comment." });
                 }
             } else
-                res.status(404).json({message: "The post with the specified ID does not exist."});
+                res.status(404).json({ message: "The post with the specified ID does not exist." });
         })
         .catch(err => {
             if(reqBody.text === undefined)
-                res.status(400).json({errorMessage: "Please provide text for the comment."});
+                res.status(400).json({ errorMessage: "Please provide text for the comment." });
             else
-                res.status(500).json({error: "There was an error while saving the comment to the database"});
+                res.status(500).json({ error: "There was an error while saving the comment to the database" });
         });
 });
 
@@ -63,8 +63,26 @@ postRouter.get('/:id', async (req, res) => {
             res.status(404).json({ message: "The post with the specified ID does not exist." })
     }
     catch{
-        res.status(500).end();
+        res.status(500).json({ error: "The post information could not be retrieved." });
     }
 });
+
+postRouter.get('/:id/comments', async (req, res) => {
+    const id = req.params.id;
+
+    try{
+        const commentArr = await db.findPostComments(id);
+
+        console.log('COMMENT ARR: ', commentArr);
+
+        if(commentArr.length > 0)
+            res.status(200).json(commentArr);
+        else   
+            res.status(404).json({ message: "The post with the specified ID does not exist." });
+    }
+    catch{
+        res.status(500).json({ error: "The comments information could not be retrieved." });
+    }
+})
 
 module.exports = postRouter;
